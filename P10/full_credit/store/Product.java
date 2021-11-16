0f6d8db5c410430ecbe01aa251958c19ac20a52e
ChauPhucNguyen <89820464+ChauPhucNguyen@users.noreply.github.com>
2021-11-16 06:48:19 -0600

    Create Product.java

---
+++ P10/full_credit/store/Product.java
@@ -0,0 +1,27 @@
+package store;
+
+public abstract class Product
+{
+ protected String name;
+ protected double price;
+ protected double cost;
+ 
+ public Product(String name, double price, double cost)
+ {
+  this.name = name;
+  this.price = price;
+  this.cost = cost;
+ }
+ 
+ public String name()
+ {
+  return name;
+ }
+ 
+ @Override
+ public String toString()
+ {
+  return "Name: " + name + "\n" + "Cost: " + cost + "\n" + "Price: " + price;
+ }
+ 
+}
